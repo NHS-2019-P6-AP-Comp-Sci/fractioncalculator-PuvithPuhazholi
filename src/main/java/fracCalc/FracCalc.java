c83f60112f4e5dd2d87b995a176970f645699a9a
s-puhazholip <s-puhazholip@53S500036573.it.bsd405.org>
2019-12-03 00:01:14 -0800

    Turning in checkpoint 3. Some of the answers are correct but are not in mixed form causing an error in the test.

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -185,10 +185,88 @@ public class FracCalc {
 			}
 		String splitString2 = "whole:" + whole + " numerator:" + numerator + " denominator:" + denominator;
 		String splitString1 = "whole:" + whole1 + " numerator:" + numerator1 + " denominator:" + denominator1;
-		return splitString2;
+		String simpOperator1 = "";
+		String simpOperator2 = "";
+		String solution = "";
+		String commDenominator = "";
+		String commNumerator1 = "";
+		String commNumerator2 = "";
+		if (numerator1.equals("0")) {
+			simpOperator1 = whole1;
+		}
+		else if (whole1.equals("0")) {
+			simpOperator1 = numerator1;
+		}
+		else if (!whole1.equals("0") && !numerator1.contentEquals("0")) {
+			simpOperator1 = String.valueOf(Integer.valueOf(whole1) * Integer.valueOf(denominator1) + Integer.valueOf(numerator1))
+					;
+		}
+		else {
+			System.out.println("ERROR. Please check the first term: " + firstTerm);
+		}
+		if (numerator.equals("0")) {
+			simpOperator2 = whole;
+		}
+		else if (whole.equals("0")) {
+			simpOperator2 = numerator;
+		}
+		else if (!whole.equals("0") && !numerator.equals("0")) {
+			simpOperator2 = String.valueOf(Integer.valueOf(whole) * Integer.valueOf(denominator) + Integer.valueOf(numerator));
+		}
+		else {
+			System.out.println("ERROR. Please check the second term: " + secondTerm);
+		}
+		
+		if (String.valueOf(operator).equals("+")) {
+			if (denominator1.equals(denominator)) {
+				solution = String.valueOf(Integer.valueOf(simpOperator1) + Integer.valueOf(simpOperator2)) + "/" + denominator;
+			 
+			}
+			else {
+				commDenominator = String.valueOf(Integer.valueOf(denominator1) * Integer.valueOf(denominator));
+				commNumerator1 = String.valueOf(Integer.valueOf(numerator1) * Integer.valueOf(denominator));
+				commNumerator2 = String.valueOf(Integer.valueOf(numerator) * Integer.valueOf(denominator1));
+				solution = String.valueOf(Integer.valueOf(commNumerator1) + Integer.valueOf(commNumerator2)) + "/" + commDenominator;
+
+			}
+		}
+		else if (String.valueOf(operator).equals("-")) {
+			if (denominator1.equals(denominator)) {
+				solution = String.valueOf(Integer.valueOf(simpOperator1) - Integer.valueOf(simpOperator2)) + "/" + denominator;
+			 
+			}
+			else {
+				commDenominator = String.valueOf(Integer.valueOf(denominator1) * Integer.valueOf(denominator));
+				commNumerator1 = String.valueOf(Integer.valueOf(numerator1) * Integer.valueOf(denominator));
+				commNumerator2 = String.valueOf(Integer.valueOf(numerator) * Integer.valueOf(denominator1));
+				solution = String.valueOf(Integer.valueOf(commNumerator1) - Integer.valueOf(commNumerator2)) + "/" + commDenominator;
+
+			}
+		}
+		else if (String.valueOf(operator).equals("*")) {
+			
+			solution = String.valueOf(Integer.valueOf(simpOperator1) * Integer.valueOf(simpOperator2)) + "/" + String.valueOf(Integer.valueOf(denominator1) * Integer.valueOf(denominator));
+			 
+		}
+		else if (String.valueOf(operator).equals("/")) {
+			
+			solution = String.valueOf(Integer.valueOf(simpOperator1) * Integer.valueOf(denominator)) + "/" + String.valueOf(Integer.valueOf(denominator1) * Integer.valueOf(simpOperator2));
+			 
+		}
+		else {
+			System.out.println("ERROR. Please check the operator: " + operator);
+		}
+		//int simpOperator2 = Integer.valueOf(whole) * Integer.valueOf(denominator) + Integer.valueOf(numerator);
+		//int solution = simpOperator1  + simpOperator2;
+		
+		System.out.println(solution);
+		System.out.println(simpOperator2);
+		return solution;
 	}
 
 	// TODO: Fill in the space below with any helper methods that you think you will
 	// need
 
-}
+	}
+
+
